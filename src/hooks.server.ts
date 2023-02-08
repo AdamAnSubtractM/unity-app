ac26452fcd76234ca2aa11d35dd0f7414f54772e
Adam Knee <adam.l.knee@gmail.com>
2023-02-08 00:15:23 -0500

    Initial commit with base styling

---
+++ src/hooks.server.ts
@@ -0,0 +1,12 @@
+// import postgres from 'postgres';
+// import type { Handle } from '@sveltejs/kit';
+
+// export const handle = (async ({ event, resolve }) => {
+// 	const sql = postgres('postgres://username:password@host:port/database');
+
+// 	event.locals = {
+// 		sql
+// 	};
+// 	const response = await resolve(event);
+// 	return response;
+// }) satisfies Handle;
