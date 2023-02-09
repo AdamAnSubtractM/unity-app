85d09c128db1075032827a1b3abb01905b725dd9
Adam Knee <aknee@irobot.com>
2023-02-09 10:54:18 -0500

    Update architecture to accomodate both API calls once the CORs is set correctly on the server.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,42 @@
-import { SECRET_HRFETCH_USER, SECRET_HRFETCH_PASS } from '$env/static/private';
+import {
+	SECRET_HRFETCH_USER,
+	SECRET_HRFETCH_PASS,
+	SECRET_ADFETCH_USER,
+	SECRET_ADFETCH_PASS
+} from '$env/static/private';
+import { PUBLIC_HRFETCH_URL, PUBLIC_ADFETCH_URL } from '$env/static/public';
 import type { LayoutLoad } from './$types';
 
+const getEncodedCredentials = function (username: string, password: string) {
+	if (!username || !password) return;
+	const credentials = `${username}:${password}`;
+	return Buffer.from(credentials).toString('base64');
+};
+
+const setEncondedCredentials = function (username: string, password: string) {
+	const credentials = getEncodedCredentials(username, password);
+	const headers = new Headers();
+	headers.append('Authorization', `Basic ${credentials}`);
+	console.log(`Credentials`, credentials);
+	console.log(`Headers`, headers);
+	return headers;
+};
+
 export const load = (async ({ fetch }) => {
-	const credentials = `${SECRET_HRFETCH_USER}:${SECRET_HRFETCH_PASS}`;
-	const headers = new Headers().append(
-		'Authorization',
-		`Basic ${Buffer.from(credentials).toString('base64')}`
-	);
-	const hrData = await fetch('http://psono.wil.contact:2275/hrdata', {
-		headers
+	const hrData = await fetch(PUBLIC_HRFETCH_URL, {
+		method: 'GET',
+		headers: setEncondedCredentials(SECRET_HRFETCH_USER, SECRET_HRFETCH_PASS)
 	});
-	const data = await hrData.json();
+	const adData = await fetch(PUBLIC_ADFETCH_URL, {
+		method: 'GET',
+		headers: setEncondedCredentials(SECRET_ADFETCH_USER, SECRET_ADFETCH_PASS)
+	});
+	console.log(`hrData`, hrData);
+	console.log(`adData`, adData);
+	const hrJSON = await hrData.json();
+	const adJSON = await adData.json();
 	return {
-		data
+		hrData: hrJSON,
+		adData: adJSON
 	};
 }) satisfies LayoutLoad;
